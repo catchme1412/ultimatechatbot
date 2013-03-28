244c23942c7f5fbbf24686940a05b66877e89c14
Rajesh KV <rajesh.k.v@orbitz.com>
2013-03-28 08:45:20 -0500

    eliza

---
+++ src/com/raj/bot/PhraseDAO.java
@@ -0,0 +1,73 @@
+package com.raj.bot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import com.raj.eliza.model.Phrase;
+
+public class PhraseDAO {
+
+    public static void create(Phrase entry) {
+        EntityManager em = EMFService.get().createEntityManager();
+        try {
+            em.persist(entry);
+        } finally {
+            em.close();
+        }
+    }
+
+    public static void delete(Phrase entry) {
+        EntityManager em = EMFService.get().createEntityManager();
+        try {
+            em.remove(entry);
+        } finally {
+            em.close();
+        }
+    }
+
+    public static void update(Phrase entry) {
+        EntityManager em = EMFService.get().createEntityManager();
+        try {
+            em.merge(entry);
+        } finally {
+            em.close();
+        }
+    }
+
+    public static Phrase find(Phrase entry) {
+        EntityManager em = EMFService.get().createEntityManager();
+        try {
+            return em.find(Phrase.class, entry);
+        } finally {
+            em.close();
+        }
+    }
+
+    public static Phrase findById(Phrase entry) {
+        EntityManager em = EMFService.get().createEntityManager();
+        try {
+            Query q = em.createQuery("select t from Phrase t where t.id=?");
+            // q.setParameter(1, entry.getId());
+            return (Phrase) q.getResultList().get(0);
+        } finally {
+            em.close();
+        }
+    }
+
+    public static List<Phrase> getLatestEntries(int start, int count) {
+        EntityManager em = EMFService.get().createEntityManager();
+        List<Phrase> todos = null;
+        try {
+            Query q = em.createQuery("select t from Phrase t");
+            // q.setFirstResult(start);
+            // q.setMaxResults(count);
+            todos = new ArrayList(q.getResultList());
+        } finally {
+            em.close();
+        }
+        return todos;
+    }
+}
--- src/com/raj/bot/UltimatechatbotServlet.java
+++ src/com/raj/bot/UltimatechatbotServlet.java
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 import com.raj.eliza.model.AuxiliaryVerb;
 import com.raj.eliza.model.Comment;
 import com.raj.eliza.model.DataLoaderUtil;
+import com.raj.eliza.model.Phrase;
+import com.raj.eliza.model.Transformer;
 
 @SuppressWarnings("serial")
 public class UltimatechatbotServlet extends HttpServlet {
@@ -20,6 +22,7 @@ public class UltimatechatbotServlet extends HttpServlet {
         resp.setContentType("text/plain");
         if ("y".equals(req.getParameter("ajax"))) {
             resp.getWriter().println("AJAX");
+            System.out.println(getAnswer("FF"));
             return;
         } else {
             if ("y".equals(req.getParameter("c")) || true) {
@@ -36,4 +39,48 @@ public class UltimatechatbotServlet extends HttpServlet {
             System.out.println(a);
         }
     }
+
+    public String getAnswer(String input) {
+        input = " " + input + " ";
+        int index = -1;
+        String upperInput = input.toUpperCase();
+        List<Comment> comments = CommentDAO.getLatestEntries(0, 100);
+        for (Comment comment : comments) {
+            for (Phrase phrase1 : comment.getPhraseGroup().getPhrases()) {
+                String phrase = phrase1.getPhraseText();
+                index = upperInput.indexOf(" " + phrase + " ");
+                if (index > -1) {
+                    String inputPhrase = adaptAuxVerbs(input.substring(index + 2 + phrase.length()));
+                    return cleanOutput(Transformer.tranform(comment.getAnswerGroup().getAnswers().iterator().next()
+                            .getAnswerText(), "*", inputPhrase));
+                }
+                ;
+            }
+            ;
+        }
+        return "";
+    }
+    
+    public static void main(String[] args) {
+        new UltimatechatbotServlet().getAnswer("FF");
+    }
+
+    public String adaptAuxVerbs(String input) {
+        String retVal = input;
+        for (AuxiliaryVerb auxVerb : AuxiliaryVerbDAO.getLatestEntries(0, 100)) {
+            retVal = Transformer.tranform(retVal, " " + auxVerb.getOriginal() + " ", " " + auxVerb.getTranformed()
+                    + "# ");
+            retVal = Transformer.tranform(retVal, " " + auxVerb.getTranformed() + " ", " " + auxVerb.getOriginal()
+                    + "# ");
+        }
+        ;
+        return Transformer.tranform(retVal, "#", "");
+    }
+
+    protected String cleanOutput(String output) {
+        return Transformer.tranform(Transformer.tranform(Transformer.tranform(
+                Transformer.tranform(Transformer.tranform(output, " .", "."), " !", "!"), " ?", "?"), " ,", ","),
+                "  ", " ");
+    }
+
 }
